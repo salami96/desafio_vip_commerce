ae601bd684f83c28fbdc142820620c00def58233
salami96 <gabriel.zanatto2@gmail.com>
2021-02-21 20:46:19 -0300

    create entities

---
+++ src/model/entities.ts
@@ -0,0 +1,27 @@
+export interface Client {
+    cod: number;
+    name: string;
+    cpf: string;
+    gender: string;
+    email: string;
+}
+export interface Product {
+    cod: number;
+    name: string;
+    color: string;
+    size: string;
+    value: string;
+}
+export interface Order {
+    cod: number;
+    date: Date;
+    obs: string;
+    payment: string;
+    cod_client: number;
+}
+export interface OrderProducts {
+    cod: number;
+    cod_order: number;
+    cod_product: number;
+    quantity: number;
+}
