00d17ccd01665359aae113028e05b5e4886dca29
salami96 <gabriel.zanatto2@gmail.com>
2021-02-22 00:17:29 -0300

    export mysql.connection & create routes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import { router } from './routes';
 import { Connection, createConnection } from 'mysql2/promise';
 
+let connection: Connection;
 export class App {
     private express: express.Application;
-    connection: Connection;
 
     constructor() {
         this.express = express();
@@ -19,14 +19,18 @@ export class App {
 
     private listen(): void {
         this.express.use('/', router);
+        this.express.listen(8000, () => {
+            console.log('Server running in port: 8000');
+        })
     }
 
     async database() {
-        const mysql = require("mysql2/promise");
-        const connection = await createConnection('mysql://root:vipcommerce@mysql-21022-0.cloudclusters.net:21022/vip');
+        const newConnection = await createConnection('mysql://root:vipcommerce@mysql-21022-0.cloudclusters.net:21022/vip');
         console.log("Conectou no MySQL!");
-        return connection;
+        connection = newConnection;
     }
 }
 
-let app = new App().getApp();
+export { connection };
+
+new App().getApp();
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,29 @@
 import { Router } from "express";
+import { ClientController } from "./controller/client.controller";
+import { OrderController } from "./controller/order.controller";
+import { ProductController } from "./controller/product.controller";
 
 const router = Router();
+
+// Rotas dos clientes
+router.get('/clientes', ClientController.getClients);
+router.get('/clientes/:cod', ClientController.getClient);
+router.post('/clientes', ClientController.saveClient);
+router.put('/clientes/:cod', ClientController.updateClient);
+router.delete('/clientes/:cod', ClientController.deleteClient);
+
+// Rotas dos produtos
+router.get('/produtos', ProductController.getProducts);
+router.get('/produtos/:cod', ProductController.getProduct);
+router.post('/produtos', ProductController.saveProduct);
+router.put('/produtos/:cod', ProductController.updateProduct);
+router.delete('/produtos/:cod', ProductController.deleteProduct);
+
+// Rotas dos pedidos
+router.get('/pedidos', OrderController.getOrders);
+router.get('/pedidos/:cod', OrderController.getOrder);
+router.post('/pedidos', OrderController.saveOrder);
+router.put('/pedidos/:cod', OrderController.updateOrder);
+router.delete('/pedidos/:cod', OrderController.deleteOrder);
 
 export { router };
